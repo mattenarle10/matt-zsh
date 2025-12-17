d530c15303086627ab7b2fc06140908a106573b9
Matthew Enarle <enarlem10@gmail.com>
2025-12-17 16:12:39 +0800

    feat: add utility plugins (autopairs, comment, lualine, which-key, gitsigns)

---
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,43 @@
+-- Other useful plugins
+return {
+  -- Auto pairs
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
+
+  -- Comment toggle
+  {
+    "numToStr/Comment.nvim",
+    config = true,
+  },
+
+  -- Status line
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "tokyonight",
+        },
+      })
+    end,
+  },
+
+  -- Which-key (shows keybindings)
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("which-key").setup()
+    end,
+  },
+
+  -- Git signs in gutter
+  {
+    "lewis6991/gitsigns.nvim",
+    config = true,
+  },
+}
