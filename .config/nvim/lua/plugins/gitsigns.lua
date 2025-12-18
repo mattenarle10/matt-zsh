3bfe082e559831279a41ebe786cc70bcd2c1219c
Matthew Enarle <enarlem10@gmail.com>
2025-12-18 09:26:08 +0800

    feat: add git signs ad blame

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,61 @@
+-- Git signs and blame
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "VeryLazy",
+  opts = {
+    signs = {
+      add          = { text = '│' },
+      change       = { text = '│' },
+      delete       = { text = '_' },
+      topdelete    = { text = '‾' },
+      changedelete = { text = '~' },
+      untracked    = { text = '┆' },
+    },
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol',
+      delay = 500,
+    },
+    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then return ']c' end
+        vim.schedule(function() gs.next_hunk() end)
+        return '<Ignore>'
+      end, {expr=true, desc="Next hunk"})
+
+      map('n', '[c', function()
+        if vim.wo.diff then return '[c' end
+        vim.schedule(function() gs.prev_hunk() end)
+        return '<Ignore>'
+      end, {expr=true, desc="Previous hunk"})
+
+      -- Actions
+      map('n', '<leader>ghs', gs.stage_hunk, {desc="Stage hunk"})
+      map('n', '<leader>ghr', gs.reset_hunk, {desc="Reset hunk"})
+      map('v', '<leader>ghs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, {desc="Stage hunk"})
+      map('v', '<leader>ghr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, {desc="Reset hunk"})
+      map('n', '<leader>ghS', gs.stage_buffer, {desc="Stage buffer"})
+      map('n', '<leader>ghu', gs.undo_stage_hunk, {desc="Undo stage hunk"})
+      map('n', '<leader>ghR', gs.reset_buffer, {desc="Reset buffer"})
+      map('n', '<leader>ghp', gs.preview_hunk, {desc="Preview hunk"})
+      map('n', '<leader>ghb', function() gs.blame_line{full=true} end, {desc="Blame line"})
+      map('n', '<leader>gb', gs.toggle_current_line_blame, {desc="Toggle line blame"})
+      map('n', '<leader>ghd', gs.diffthis, {desc="Diff this"})
+      map('n', '<leader>ghD', function() gs.diffthis('~') end, {desc="Diff this ~"})
+
+      -- Text object
+      map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', {desc="Select hunk"})
+    end
+  },
+}
--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -49,9 +49,77 @@ return {
     end,
   },
 
-  -- Git signs in gutter
+  -- Surround text objects
   {
-    "lewis6991/gitsigns.nvim",
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
     config = true,
+  },
+
+  -- Better diagnostics and quickfix
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "Trouble",
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>xl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)" },
+      { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+    },
+    opts = {},
+  },
+
+  -- Fast navigation with 2-char search
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    },
+  },
+
+  -- Indent guides
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = "VeryLazy",
+    opts = {
+      indent = {
+        char = "│",
+      },
+      scope = {
+        enabled = true,
+        show_start = false,
+        show_end = false,
+      },
+    },
+  },
+
+  -- Quick file navigation
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup()
+
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon add file" })
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon menu" })
+      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = "Harpoon file 1" })
+      vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end, { desc = "Harpoon file 2" })
+      vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end, { desc = "Harpoon file 3" })
+      vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end, { desc = "Harpoon file 4" })
+    end,
+  },
+
+  -- Auto-detect indentation
+  {
+    "tpope/vim-sleuth",
   },
 }
